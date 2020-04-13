12cda899f63f85242ca0ed335f6d4f44591ba324
Frank Azandegbe <beninbenino@gmail.com>
2020-04-13 17:16:53 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+function objectToMyModel($array_iteam,$model){
+    foreach($array_iteam as $key => $value) {
+        $model[$key] =$value;
+    }return $model;
+}
+if (file_exists('200406104654_151394_2.XML')) {
+    $xml = simplexml_load_file('200406104654_151394_2.XML');
+    $json = json_encode($xml);
+    $array = json_decode($json);
+    //print_r($array->POSTOBJECT->Id);
+    //print_r($array->POSTOBJECT->AC->SMDOCUMENTS);
+    //print_r($array->POSTOBJECT->AC);
+    $arrayModels = array();
+    foreach($array->POSTOBJECT as $postObject){
+        $model = array();
+        foreach ($postObject->AC as $value) {
+            $model = objectToMyModel($value,$model);
+        }
+        array_push($arrayModels,(object)$model);
+    }
+    print_r($arrayModels);
+    //objectToMyModel($array->POSTOBJECT->AC->SMDOCUMENTS);
+} else {exit('Не удалось открыть файл 200406104654_151394_2.XML.');}
+?>
